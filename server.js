7fa30fee88051288c03fe1374a91fc192e1cb74f
arjun391c <arjun391c@gmail.com>
2020-06-16 22:28:11 +0530

    serialization of passport user

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const session = require("express-session");
 const passport = require("passport");
+const mongo = require("mongodb").MongoClient;
+//for user id
+const ObjectID = require("mongodb").ObjectID;
 
 require('dotenv').config()
 
@@ -20,20 +23,46 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //express sessions
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-}));
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+  }
+));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.render("pug/index", {title: 'Hello', message: 'Please login'});
- // res.send(`Pug template is not defined.`);
+//database connection
+mongo.connect(process.env.DATABASE, { useUnifiedTopology: true }, (err, db) => {
+  if(err) {
+    console.log('Database error: ' + err);
+  } else {
+    console.log('Successful database connection');
+
+    //serialization and app.listen
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    });
+
+    passport.deserializeUser( (id, done) => {
+        db.collection('users').findOne(
+            {_id: new ObjectID(id)},
+            (err, doc) => {
+                done(null, doc);
+            }
+        );
+    });
+    
+    app.route("/").get((req, res) => {
+      //Change the response to render the Pug template
+      res.render("pug/index", {title: 'Hello', message: 'Please login'});
+     // res.send(`Pug template is not defined.`);
+    });
+    
+    app.listen(process.env.PORT || 3000, () => {
+      console.log("Listening on port " + process.env.PORT);
+    });
+  }
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+

b19053c063aa5fa770f3ed74a1238d3c381e6663
arjun391c <arjun391c@gmail.com>
2020-06-16 21:20:38 +0530

    pug as view engine

--- server.js
+++ server.js
@@ -6,13 +6,17 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
+//view engine
+app.set("view engine", "pug");
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route("/").get((req, res) => {
   //Change the response to render the Pug template
-  res.send(`Pug template is not defined.`);
+  res.render("pug/index");
+ // res.send(`Pug template is not defined.`);
 });
 
 app.listen(process.env.PORT || 3000, () => {

cba0981abd8598032627d88bfd9321e83737c186
arjun391c <arjun391c@gmail.com>
2020-06-17 16:27:13 +0530

    Oauth github strategy

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ const session = require("express-session");
 const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
 const LocalStrategy = require("passport-local");
+const GitHubStrategy = require("passport-github").Strategy;
 const bcrypt = require("bcrypt");
 
 require("dotenv").config();
@@ -48,5 +49,41 @@ module.exports = function (app, db) {
         return done(null, user);
       });
     })
+  );
+
+  //github strategy
+  passport.use(
+    new GitHubStrategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL:
+          "https://wakeful-outrageous-fascinator.glitch.me/auth/github/callback",
+      },
+      function (accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+        //Database logic here with callback containing our user object
+        db.collection('socialusers').findAndModify(
+            {id: profile.id},
+            {},
+            {$setOnInsert:{
+              id: profile.id,
+              name: profile.displayName || 'John Doe',
+              photo: profile.photos[0].value || '',
+              email: profile.emails[0].value || 'No public email',
+              created_on: new Date(),
+              provider: profile.provider || ''
+            },$set:{
+              last_login: new Date()
+            },$inc:{
+              login_count: 1
+            }},
+            {upsert:true, new: true},
+            (err, doc) => {
+              return cb(null, doc.value);
+            }
+          );
+      }
+    )
   );
 };
